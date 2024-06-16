90b1878122853acb8a82597d7b855d6c31d95dc3
praveenlodhi99 <praveenlodhi.official@gmail.com>
2024-06-17 02:10:48 +0530

    Project 01 - Animated Cursor, Finally Completed 😙

--- Project 01 - Animated Cursor/script.js
+++ Project 01 - Animated Cursor/script.js
@@ -17,6 +17,30 @@ main.addEventListener("mousemove", function (event) {
     });
 });
 
+// Add event listener for image hover effects
+imageDiv.addEventListener("mouseenter", function () {
+    cursor.textContent = "View More"; // Change cursor content
+    gsap.to(cursor, {
+        scale: 3, // Scale cursor up on hover
+        duration: 0.8,
+        ease: "back.out",
+        border: "0.2px dashed white",
+        backgroundColor: "rgba(255, 255, 255, 0.4)", // Corrected RGBA value
+        opacity: 0.8, // Set the opacity value
+        boxShadow: "2px 2px 8px rgba(0, 0, 0, 1.2), -1px -1px 8px rgba(255, 255, 255, 0.5)" // Corrected property
+    });
+});
+
+
+imageDiv.addEventListener("mouseleave", function () {
+    cursor.textContent = ""; // Reset cursor content
+    gsap.to(cursor, {
+        scale: 1, // Scale cursor back to normal
+        duration: 0.8,
+        ease: "back.out",
+        backgroundColor: "white"
+    });
+});
 
 
 
